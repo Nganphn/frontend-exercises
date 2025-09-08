c6e29b248cd55ede46bc6cada80dbb487b631408
Ngan Pham <phnhungan454@gmail.com>
2025-09-08 19:55:24 +0000

    e01 practice01

---
+++ e01-react-basics/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import Practice01App from "./ex01/Practice01App.jsx";
+
+function App() {
+  return <Practice01App />;
+}
+
+export default App;
